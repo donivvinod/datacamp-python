38d2424e05be5b93b180526b5d0148b0f4d7dec7
Vinod Kumar <vinodkumar.vasudevan@gmail.com>
2018-06-24 18:47:20 +0800

    Added dictionary related sample

---
+++ dictionary.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin', 'norway':'oslo' }
+
+# Add italy to europe
+europe['italy'] ='rome'
+
+# Print out italy in europe
+print('italy' in europe)
+
+# Add poland to europe
+europe['poland'] ='warsaw'
+
+# Remove norway
+del europe['norway']
+
+# Print europe
+print(europe)
+
+# Dictionary of dictionaries
+europe2 = { 'spain': { 'capital':'madrid', 'population':46.77 },
+           'france': { 'capital':'paris', 'population':66.03 },
+           'germany': { 'capital':'berlin', 'population':80.62 },
+           'norway': { 'capital':'oslo', 'population':5.084 } }
+
+# Print out the capital of France
+print(europe2['france']['capital'])
+
+# Create sub-dictionary data
+data = { 'capital':'rome', 'population':59.83 } 
+
+# Add data to europe under key 'italy'
+europe2['italy'] = data
+
+# Print europe
+print(europe2)
